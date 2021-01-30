5c625b889ec480279a73301f235cf59818ba73e1
AemieJ <aemie.j@hotmail.co.uk>
2021-01-30 14:25:20 +0400

    feat: add flow for code validation

--- extension/src/popup/popup.js
+++ extension/src/popup/popup.js
@@ -7,6 +7,26 @@ function showNewUserStuff() {
     .forEach((e) => (e.style.display = "none"));
 }
 
+function showCodeNotSet() {
+  document
+    .querySelectorAll(".code-not-set")
+    .forEach((e) => (e.style.display = "block"));
+  document
+    .querySelectorAll(".code-set")
+    .forEach((e) => (e.style.display = "none"));
+}
+
+function showCodeSet() {
+  const code = localStorage.getItem("code");
+  document
+    .querySelectorAll(".code-set")
+    .forEach((e) => (e.style.display = "block"));
+  document
+    .querySelectorAll(".code-not-set")
+    .forEach((e) => (e.style.display = "none"));
+  document.querySelector("input#code").value = code;
+}
+
 function showOldUserStuff(user) {
   document
     .querySelectorAll(".old-user-stuff")
@@ -16,10 +36,26 @@ function showOldUserStuff(user) {
     .forEach((e) => (e.style.display = "none"));
   document.querySelector("#user-name").innerHTML = user.name;
   document.querySelector("#user-email").innerHTML = user.email;
+
+  if (localStorage.getItem("code") == null) {
+    showCodeNotSet();
+  } else {
+    showCodeSet();
+  }
+}
+
+function setCode(code) {
+  localStorage.setItem("code", code);
+  displayUserStuff();
 }
 
 function setUser(user) {
   localStorage.setItem("user", JSON.stringify(user));
+  displayUserStuff();
+}
+
+function removeCode() {
+  localStorage.removeItem("code");
   displayUserStuff();
 }
 
@@ -71,6 +107,20 @@ window.onload = () => {
 
   document.querySelector("button#logout").addEventListener("click", () => {
     removeUser();
+  });
+
+  document.querySelector("button#save").addEventListener("click", () => {
+    const val = document.querySelector("input#code").value;
+    if (val !== '') {
+      setCode(val);
+    } else {
+      removeCode();
+    }
+  });
+
+  document.querySelector("button#clear").addEventListener("click", () => {
+    document.querySelector("input#code").value = '';
+    removeCode();
   });
 
   displayUserStuff();
