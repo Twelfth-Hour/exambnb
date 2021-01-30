c468a739f9ddcd3cd59866ae235950fe77c1dbdb
Sahil Bondre <sahilbondre@gmail.com>
2021-01-30 14:38:30 +0530

    feat: setup conditional rendering
    Co-authored-by: Aemie <aemie.j@hotmail.co.uk>

--- extension/src/popup/popup.js
+++ extension/src/popup/popup.js
@@ -1,3 +1,42 @@
+function showNewUserStuff() {
+  document
+    .querySelectorAll(".new-user-stuff")
+    .forEach((e) => (e.style.display = "block"));
+  document
+    .querySelectorAll(".old-user-stuff")
+    .forEach((e) => (e.style.display = "none"));
+}
+
+function showOldUserStuff() {
+  document
+    .querySelectorAll(".old-user-stuff")
+    .forEach((e) => (e.style.display = "block"));
+  document
+    .querySelectorAll(".new-user-stuff")
+    .forEach((e) => (e.style.display = "none"));
+}
+
+function setUser(user) {
+  localStorage.setItem("user", JSON.stringify(user));
+  console.log(user);
+  document.querySelector("#user-name").innerHTML = user.name;
+  document.querySelector("#user-email").innerHTML = user.email;
+  displayUserStuff();
+}
+
+function removeUser() {
+  localStorage.removeItem("user");
+  displayUserStuff();
+}
+
+function displayUserStuff() {
+  if (localStorage.getItem("user") == null) {
+    showNewUserStuff();
+  } else {
+    showOldUserStuff();
+  }
+}
+
 window.onload = () => {
   document.querySelector("button#sign-up").addEventListener("click", () => {
     chrome.identity.getAuthToken({ interactive: true }, (token) => {
@@ -21,8 +60,17 @@ window.onload = () => {
         requestOptions
       )
         .then((response) => response.json())
-        .then((result) => console.log(result))
-        .catch((error) => console.log("error", error));
+        .then((result) => setUser(result))
+        .catch((error) => {
+          removeUser();
+          console.log("error", error);
+        });
     });
   });
+
+  document.querySelector("button#logout").addEventListener("click", () => {
+    removeUser();
+  });
+
+  displayUserStuff();
 };
