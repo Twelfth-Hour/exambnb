c259dd8a84f68d05232f0fa8b08242112920be01
Sahil Bondre <sahilbondre@gmail.com>
2021-01-30 12:07:50 +0530

    feat: setup key

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,5 +1,3 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#3aa757" }, function () {
-    console.log("The color is very green.");
-  });
+chrome.browserAction.onClicked.addListener(function () {
+  chrome.tabs.create({ url: "index.html" });
 });
